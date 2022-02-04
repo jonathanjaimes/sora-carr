14dea3727d88dbe722b27628af7e919a7dc33b26
Jonathan Jaimes <jonathanguillermojm@ufps.edu.co>
2022-02-04 16:07:15 -0500

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
   title = 'carr';
+  
+  bandera1: boolean = true;
+  bandera2: boolean = false;
+  bandera3: boolean = false
+
+  cambiar() {
+    this.bandera1 = true;
+    this.bandera2 = false;
+    this.bandera3 = false;
+  }
+
+  cambiar2() {
+    this.bandera1 = false;
+    this.bandera2 = true;
+    this.bandera3 = false;
+  }
+
+  cambiar3() {
+    this.bandera1 = false;
+    this.bandera2 = false;
+    this.bandera3 = true;
+  }
+  
+
 }
